42cbc00effb6825c5e34913ea36e34048c106fd7
Jake Pelrah <pelrahjsp@gmail.com>
2024-08-29 15:21:40 -0400

    remove api key

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,5 @@
 import * as Carousel from "./Carousel.js";
 import { API_KEY } from "../apiKey.js";
-// const API_KEY = "live_4MYP3nZ58BfE8rmxZuATDsNPzOcz28uVAdAwyrNmdtPWtnVIYNBWu4wuCpP3QlBp";
 const API_BASE_URL = 'https://api.thecatapi.com/v1';
 const API_IMAGE_URL = 'images/search?limit=10&breed_ids='
 const headers = { 'x-api-key': API_KEY }
